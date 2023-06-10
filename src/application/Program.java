82e080542df29372fe69f6c1dbdc5eda043ab8da
Erick Yuta <erick_miyagi34231@hotmail.com>
2023-06-10 12:37:44 -0300

    add move on pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		ChessMatch chessMath = new ChessMatch();
-		UI.printBoard(chessMath.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMath.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(scanner);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(scanner);
+			
+			ChessPiece capturedPiece = chessMath.performChessPiece(source, target);
+		}
 		
 	}
 }
